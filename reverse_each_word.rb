1a5b3c9b63e38b2efd8ea41b52e34729bfc22368
KeslieBox <kesliebox@gmail.com>
2021-01-15 20:28:33 -0600

    commit tree 56fe4f912e1e794a432aadb75ed5f7377f490ab1

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,23 @@
 require 'pry'
-
+#these all work
 def reverse_each_word(sentence)
-    sentence_array = sentence.split.collect {|word| word.reverse}.join(" ")
+    sentence.split.collect {|word| word.reverse}.join(" ")
 end
+
+# def reverse_each_word(sentence)
+#    sentence_array = sentence.split.collect do |word| 
+#         word.reverse
+#     end.join(" ")
+# end
+
+# def reverse_each_word(sentence)
+#     sentence_array = sentence.split
+#     collect_array = sentence_array.collect do |word|
+#         word.reverse
+#     end
+#     collect_array.join(" ")
+# end
+ 
 
 
 
